a6e1220b8f4414ee3b3421bc2c764cd07da4f54e
aaartyomsss <59570358+aaartyomsss@users.noreply.github.com>
2020-11-06 19:44:02 +0200

    cors

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv/config')
 const app = express();
+const cors = require('cors')
 
 
 // Import Routes
 const todosRoute = require('./routes/todos')
 
 //Middleware
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
@@ -18,4 +20,5 @@ app.use('/todos', todosRoute)
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 //Listening to the server
-app.listen(3000)
+const PORT = 3001
+app.listen(PORT)
